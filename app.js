5d53ecb83a85d29f0a3703bba000415e2fd5d8a9
Shuyang Chen <sc1085@gmail.com>
2016-03-01 16:19:48 -0500

    Version 1.0.1 - Added command line options

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-/// <reference path="typings/tsd.d.ts" />
+/// <reference path="typings/main.d.ts" />
 'use strict';
 
 const path = require('path');
@@ -8,24 +8,14 @@ const phantomjs = require('phantomjs-prebuilt');
 const phantom = require('phantom');
 const prompt = require('prompt');
 const async = require('async');
+const minimist = require('minimist');
 
 const phantomPathSlashes = phantomjs.path.split(/\/|\\/);
 const phantomPath = phantomjs.path.substr(0, phantomjs.path.length - phantomPathSlashes[phantomPathSlashes.length - 1].length);
 
 const loginUrl = 'https://login.gatech.edu/cas/login?service=https%3A%2F%2Ft-square.gatech.edu%2Fsakai-login-tool%2Fcontainer';
 
-function getSites() { // Will not get all sites if is selecting something other than My Workspace
-	var links = document.querySelectorAll('#siteLinkList a');
-	var tabs = {};
-	for (var i in links) {
-		var link = links[i];
-		var span = link.firstElementChild;
-		if (span && span.textContent) {
-			tabs[span.textContent] = link.href;
-		}
-	}
-	return tabs;
-}
+const argv = minimist(process.argv.slice(2));
 
 function getSelectedSite(sites) {
 	for (var i in sites) {
@@ -33,19 +23,6 @@ function getSelectedSite(sites) {
 			return i;
 		}
 	}
-}
-
-function getTools() { // Will not get all tools if is selecting something other than Home
-	var links = document.querySelectorAll('#toolMenu a');
-	var tools = {};
-	for (var i in links) {
-		var link = links[i];
-		var span = link.firstElementChild;
-		if (span && span.textContent) {
-			tools[span.textContent] = link.href;
-		}
-	}
-	return tools;
 }
 
 function getSelectedTool(tools) {
@@ -56,37 +33,23 @@ function getSelectedTool(tools) {
 	}
 }
 
-function getAssignmentIFrameSrc() {
-	return document.querySelector('.portletMainWrap iframe').src;
-}
-
-function getAssignments() {
-	var trs = document.querySelectorAll('.portletBody table tr');
-	
-	var assignments = [];
-	
-	if (trs.length < 2) {
-		return assignments;
-	}
-	
-	for (var i = 1; i < trs.length; i++) {
-		var tr = trs[i];
-		var children = tr.children;
-		var assignment = {};
-		for (var j = 0; j < children.length; j++) {
-			var td = children[j];
-			if (td.headers) {
-				assignment[td.headers] = td.textContent.trim();
-			}
-		}
-		assignments.push(assignment);
-	}
-	return assignments;
-}
-
 let phan = null;
 
 async.waterfall([
+	(done) => {
+		phantom.create({
+			path: phantomPath,
+			parameters: {
+				'web-security': 'no'
+			},
+			dnodeOpts: {
+				weak: false
+			}
+		}, (ph) => {
+			phan = ph;
+			done(null);
+		});
+	},
 	(done) => {
 		prompt.start();
 		prompt.get({
@@ -99,36 +62,24 @@ async.waterfall([
 					hidden: true
 				}
 			}
-		}, done);
-	},
-	(result, done) => {
-		phantom.create({
-			path: phantomPath,
-			parameters: {
-				'web-security': 'no'
-			},
-			dnodeOpts: {
-				weak: false
-			}
-		}, (ph) => {
-			phan = ph;
-			done(null, ph, result);
-		})
-	},
-	(ph, auth, done) => {
-		ph.createPage((page) => {
-			done(null, ph, auth, page);
+		}, (error, result) => {
+			done(error, result);
 		});
 	},
-	(ph, auth, page, done) => {
+	(auth, done) => {
+		phan.createPage((page) => {
+			done(null, auth, page);
+		});
+	},
+	(auth, page, done) => {
 		console.log('Logging in...');
 		page.open(loginUrl, (status) => {
-			done(status == 'success' ? null : status, ph, auth, page);
+			done(status == 'success' ? null : status, auth, page);
 		});
 	},
-	(ph, auth, page, done) => {
+	(auth, page, done) => {
 		page.set('onLoadFinished', (success) => {
-			done(success == 'success' ? null : success, ph, page);
+			done(success == 'success' ? null : success, page);
 		});
 		page.evaluate(function (auth) {
 			var form = document.querySelector('form');
@@ -138,22 +89,34 @@ async.waterfall([
 		}, () => {
 		}, auth);
 	},
-	(ph, page, done) => {
+	(page, done) => {
 		console.log('Logged in');
 		page.set('onLoadFinished', null);
-		page.evaluate(getSites, (sites) => {
-			let classes = [];
-			for (let i in sites) {
+		page.evaluate(function () {
+			// Will not get all sites if is selecting something other than My Workspace
+			var links = document.querySelectorAll('#siteLinkList a');
+			var tabs = {};
+			for (var i in links) {
+				var link = links[i];
+				var span = link.firstElementChild;
+				if (span && span.textContent) {
+					tabs[span.textContent] = link.href;
+				}
+			}
+			return tabs;
+		}, (sites) => {
+			const classes = [];
+			for (const i in sites) {
 				if (i != 'My Workspace') {
 					classes.push(i);
 				}
 			}
-			// console.log(classes);
-			let all = {};
+			const all = {};
 			async.eachSeries(classes, (site, next) => {
-				console.log(`Parsing assignments for ${site}`);
-				let siteUrl = sites[site];
-				// console.log(siteUrl);
+				if (argv.v) {
+					console.log(`\n#${site}`);
+				}
+				const siteUrl = sites[site];
 				async.waterfall([
 					(step) => {
 						page.open(siteUrl, (status) => {
@@ -161,13 +124,24 @@ async.waterfall([
 						});
 					},
 					(step) => {
-						page.evaluate(getTools, (tools) => {
+						page.evaluate(function () {
+							// Will not get all tools if is selecting something other than Home
+							var links = document.querySelectorAll('#toolMenu a');
+							var tools = {};
+							for (var i in links) {
+								var link = links[i];
+								var span = link.firstElementChild;
+								if (span && span.textContent) {
+									tools[span.textContent] = link.href;
+								}
+							}
+							return tools;
+						}, (tools) => {
 							step(null, tools);
 						});
 					},
 					(tools, step) => {
-						// console.log(tools);
-						let assignment = tools['Assignments'];
+						const assignment = tools['Assignments'];
 						if (assignment) {
 							async.waterfall([
 								(forward) => {
@@ -176,7 +150,9 @@ async.waterfall([
 									});
 								},
 								(forward) => {
-									page.evaluate(getAssignmentIFrameSrc, (iframe) => {
+									page.evaluate(function () {
+										return document.querySelector('.portletMainWrap iframe').src;
+									}, (iframe) => {
 										forward(null, iframe);
 									});
 								},
@@ -186,8 +162,47 @@ async.waterfall([
 									});
 								},
 								(forward) => {
-									page.evaluate(getAssignments, (assignments) => {
-										// console.log(assignments);
+									page.evaluate(function () {
+										var trs = document.querySelectorAll('.portletBody table tr');
+										
+										var assignments = [];
+										
+										if (trs.length < 2) {
+											return assignments;
+										}
+										
+										for (var i = 1; i < trs.length; i++) {
+											var tr = trs[i];
+											var children = tr.children;
+											var assignment = {};
+											for (var j = 0; j < children.length; j++) {
+												var td = children[j];
+												if (td.headers) {
+													assignment[td.headers] = td.textContent.trim();
+												}
+											}
+											assignments.push(assignment);
+										}
+										return assignments;
+									}, (assignments) => {
+										let first = !argv.v;
+										const now = Date.now();
+										assignments.forEach((assignment) => {
+											const diff = Date.parse(assignment.dueDate) - now;
+											if (diff > 0) {
+												if (first) {
+													console.log(`\n#${site}`);
+													first = false;
+												}
+												if (argv.d) {
+													console.log(`${assignment.title} - ${assignment.dueDate}`);
+												} else {
+													const days = Math.floor(diff / (60 * 60 * 1000 * 24));
+													const hours = Math.floor((diff / (60 * 60 * 1000)) % 24);
+													console.log(`${assignment.title} due in ${days} ${days == 1 ? 'day' : 'days'} and ${hours} ${hours == 1 ? 'hour' : 'hours'}`);
+												}
+											}
+										});
 										all[site] = assignments;
 										forward();
 									});
@@ -213,21 +228,5 @@ async.waterfall([
 	}
 	if (error) {
 		return console.error(error);
-	}
-	
-	const now = Date.now();
-	
-	console.log('Printing assignments...');
-	
-	for (let site in all) {
-		const assignments = all[site];
-		assignments.forEach((assignment) => {
-			const diff = Date.parse(assignment.dueDate) - now;
-			if (diff > 0) {
-				const days = Math.floor(diff / (60 * 60 * 1000 * 24));
-				const hours = Math.floor((diff / (60 * 60 * 1000)) % 24);
-				console.log(`${site} ${assignment.title} due in ${days} ${days == 1 ? 'day' : 'days'} and ${hours} ${hours == 1 ? 'hour' : 'hours'}`);
-			}
-		});
 	}
 });
